b65cba0c0175dc63088bead6989f6da40e187141
Daisuke <okka0727@gmail.com>
2026-03-02 21:37:35 +0900

    jsonファイルの場所を修正

--- python/config.py
+++ python/config.py
@@ -2,7 +2,7 @@
 SPREADSHEET_KEY = '1eYvBli5lOdl991ZSkinb3Bvhrrw3KqGeQLzfedHyITY'
 
 # 塾のプロジェクトで発行したJSONファイル名
-JSON_FILE = 'credentials.json'
+JSON_FILE = 'python/credentials.json'
 
 # GitHub Pagesなどの公開URL
 STUDENT_URL_BASE = 'https://okamuro-d.github.io/Risshi-Log/web/index.html?id='
