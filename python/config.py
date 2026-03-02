3daffce37d45a995d918d3d6bbbdc551af862ae9
Daisuke <okka0727@gmail.com>
2026-03-02 20:42:04 +0900

    Update: Change STUDENT_URL_BASE to GitHub Pages

--- python/config.py
+++ python/config.py
@@ -5,4 +5,4 @@ SPREADSHEET_KEY = '1eYvBli5lOdl991ZSkinb3Bvhrrw3KqGeQLzfedHyITY'
 JSON_FILE = 'credentials.json'
 
 # GitHub Pagesなどの公開URL
-STUDENT_URL_BASE = 'https://risshi-log-test.netlify.app/?id='
+STUDENT_URL_BASE = 'https://okamuro-d.github.io/Risshi-Log/web/index.html?id='
